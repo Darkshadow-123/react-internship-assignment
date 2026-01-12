8c1e10ae718b1bae581e3b5fd51378a7c1e314e1
Rishi Lalwani <Rishi.123.lalwani@gmail.com>
2026-01-13 02:11:37 +0530

    .

--- src/api/artworks.api.ts
+++ src/api/artworks.api.ts
@@ -1,6 +1,6 @@
 import type{ ArtworksApiResponse } from '../types/api-response.types';
 
-const BASE_URL = import.meta.env.BASE_URL;
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const fetchArtworksByPage = async (
   page: number
